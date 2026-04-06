ccbe17c20e2f7cbc6da3ce2d1d2ccc025745b87e
tonyyarak03 <tonyyarak03@gmail.com>
2026-04-07 00:11:47 +0300

    fix mongodb

--- server.js
+++ server.js
@@ -2,28 +2,30 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const authRoutes = require("./routes/auth"); // 👈 KEEP THIS HERE
+const authRoutes = require("./routes/auth");
 
-const app = express(); // 👈 MUST BE BEFORE app.use
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// ROUTES
 app.use("/api/auth", authRoutes);
 
-// DB
-//mongoose.connect("mongodb://127.0.0.1:27017/futuris");
-console.log("MongoDB disabled for deployment");
-
-// TEST ROUTE
 app.get("/", (req, res) => {
   res.send("Futuris backend running");
 });
 
-// START
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+
+    app.listen(PORT, () => {
+      console.log("Server running on port " + PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error:", err.message);
+  });
